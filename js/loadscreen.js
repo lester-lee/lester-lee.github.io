85f9fb4f60e0ab11f9cd7ee1f951d1447d620610
Lester Lee <ll6@williams.edu>
2016-05-25 14:12:09 -0400

    added a load screen

---
+++ js/loadscreen.js
@@ -0,0 +1,20 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+	$("#enterload").html("enter");
+	$("#enterload").click(function () {
+		$("#textload, #enterload").hide();
+		$("leftload").addClass("away");
+		$("#leftload").animate({
+			left: '-50%'
+		}, 800);
+		$("#rightload").animate({
+			left: '+50%'
+		}, 800);
+		setTimeout(function () {
+			//$("#leftload").css("display", "none");
+			$("#leftload").addClass("noshow");
+		}, 800);
+		$("body").css("overflow-y", "auto");
+	});
+});
