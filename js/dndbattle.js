8ceffac2f824744b77ee3a097c7572002ebdcf0e
Lester Lee <ll6@williams.edu>
2016-06-08 23:40:48 -0400

    typo

--- js/dndbattle.js
+++ js/dndbattle.js
@@ -38,7 +38,7 @@ function addCharBox() {
 	var charDiv = '<div class="character" id="name' + name + init +
 		'" data-init="init' + init + '">' +
 		'<h3><span class="char-name">' + name + '</span><input type="button" class="remove-char" ></input>' +
-		'<input type="button" class="dead-check"></input' +
+		'<input type="button" class="dead-check"></input>' +
 		'<input type="button" class="open-preset" ></input></h3>' +
 		'<div class="char-stats">' +
 		'<textarea class="ac" placeholder="AC"></textarea>' +
