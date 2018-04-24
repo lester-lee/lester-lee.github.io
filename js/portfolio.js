48e847de8f05388235987dfb2a41a1e741b13ca7
Lester Lee <ll6@williams.edu>
2018-04-23 22:09:17 -0400

    added photoswipe

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,4 +3,32 @@ $(document).ready( function() {
     itemSelector: 'li',
     columnWidth: 300
   });
+
+  /* Photoswipe */
+  $("#gallery-container li img").on("click", function(){
+    console.log($(this));
+    console.log(gallery);
+
+    var pswpElem = $(".pswp")[0];
+
+    var images = [];
+    $("#gallery-container li img").each(function(){
+      var img = $(this)[0];
+      var imgObj = {
+        src: img.src,
+        w: img.naturalWidth,
+        h: img.naturalHeight
+      };
+      images.push(imgObj);
+    });
+
+
+    var options = {
+      index: $(this).attr("data-idx") - 1,
+      shareEl: false
+    };
+
+    var gallery = new PhotoSwipe( pswpElem, PhotoSwipeUI_Default, images, options);
+    gallery.init();
+  });
 });
