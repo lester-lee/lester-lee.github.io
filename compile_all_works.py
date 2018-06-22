b03f97cf6e2eeac951638f1e4d4c3340657fbb45
Lester Lee <ll6@williams.edu>
2018-06-22 19:17:17 -0400

    works showing up on index

---
+++ compile_all_works.py
@@ -0,0 +1,16 @@
+import os, yaml
+
+data_dir = "_data/works/"
+
+
+all_works = []
+
+for fname in os.listdir(data_dir):
+    gallery = fname.split(".")[0]
+    with open(data_dir+fname) as f:
+        works = yaml.load(f)
+        for w in works:
+            w["gallery"] = gallery
+        all_works += works
+
+yaml.dump(all_works, open("_data/all_works.yml", "w"))
