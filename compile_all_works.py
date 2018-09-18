867687ebcb61caedb339c78d77a4a1652f9b6740
Lester Lee <ll6@williams.edu>
2018-09-18 11:20:33 -0400

    Update index page

--- compile_all_works.py
+++ compile_all_works.py
@@ -9,8 +9,9 @@ for fname in os.listdir(data_dir):
     gallery = fname.split(".")[0]
     with open(data_dir+fname) as f:
         works = yaml.load(f)
-        for w in works:
-            w["gallery"] = gallery
-        all_works += works
+        if works:
+            for w in works:
+                w["gallery"] = gallery
+            all_works += works
 
 yaml.dump(all_works, open("_data/all_works.yml", "w"))
