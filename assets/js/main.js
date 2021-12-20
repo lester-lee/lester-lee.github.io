5c737deb6f1a2982a5177fd9a6307d5c0a888812
Lester Lee <cs@lester-lee.com>
2021-12-19 21:16:03 -0500

    Add googly eyes

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,11 +32,11 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 });
 
 // Keep track of active article
-let activeArticle;
-
+let activeArticle = document.querySelector("#Welcome");
 const updateActiveArticle = (article) => {
   if (activeArticle == article) return;
 
+  activeArticle = article;
   const activeId = article.getAttribute("id");
   const color = article.getAttribute("data-color");
 
@@ -51,6 +51,11 @@ const updateActiveArticle = (article) => {
   activeNav.firstChild.style.borderColor = COLORS[color];
 };
 
+// Draw box around link on page load; kinda hacky
+document.querySelector(`a[href^="#Welcome"]`).firstChild.style.borderColor =
+  COLORS["yellow"];
+
+// Update active article on scroll
 let lastPosition = 0;
 main.addEventListener("scroll", (e) => {
   const position = main.scrollTop;
@@ -73,4 +78,38 @@ main.addEventListener("scroll", (e) => {
 
   lastPosition = position <= 0 ? 0 : position;
 });
+
 //#endregion Navigation
+
+//-----------------------------
+//#region Drawings Animation
+//-----------------------------
+// Make portraits tilt toward mouse
+// Code from https://www.armandocanals.com/posts/CSS-transform-rotating-a-3D-object-perspective-based-on-mouse-position.html
+const constraint = 20;
+
+const transforms = (x, y, elem) => {
+  const box = elem.getBoundingClientRect();
+  const cX = -(y - box.y - box.height / 2) / constraint;
+  const cY = (x - box.x - box.width / 2) / constraint;
+
+  return `perspective(400px) rotateX(${cX}deg) rotateY(${cY}deg)`;
+};
+
+const transformElement = (elem, xyelem) => {
+  elem.style.transform = transforms.apply(null, xyelem);
+};
+
+main.addEventListener("mousemove", (e) => {
+  const activePortrait = document.querySelector(
+    `#${activeArticle.id} .Eyes`
+  );
+  const xy = [e.clientX, e.clientY];
+  const position = xy.concat([activePortrait]);
+
+  window.requestAnimationFrame(() => {
+    transformElement(activePortrait, position);
+  });
+});
+
+//#endregion Drawings Animation
